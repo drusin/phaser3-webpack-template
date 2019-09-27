ae729a35f6ea3aecd87dc53f15ce513e08d7a09f
Dawid Rusin <d.rusin@topdesk.com>
2019-09-27 09:50:32 +0200

    Added small example game

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('Hello, world!');
+import Phaser from 'phaser';
+import GameScene from './GameScene';
+
+const config = {
+	type: Phaser.AUTO,
+	pixelArt: true,
+	parent: 'canvas-parent',
+	physics: {
+		default: 'arcade',
+		arcade: { debug: true }
+	},
+	scene: [GameScene]
+};
+
+new Phaser.Game(config);
